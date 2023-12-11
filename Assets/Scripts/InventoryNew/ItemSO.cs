0b124e7b492f6e33f314dbcd60c09f2a98a9bf82
RaynaldiAditya <110702991+RayZTl@users.noreply.github.com>
2023-12-11 22:10:30 +0700

    Inventory

---
+++ Assets/Scripts/InventoryNew/ItemSO.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public StatToChange statToChange = new StatToChange();
+    public int amountToChangeStat;
+    public AttributeToChange attributeToChange = new AttributeToChange();
+    public int amountToChangeAttribute;
+
+    public bool UseItem()
+    {
+        if(statToChange == StatToChange.health) 
+        {
+            HealthPlayer health = GameObject.Find("player").GetComponent<HealthPlayer>();
+            if(health.currentHealth == health.maxhealth)
+            {
+                return false;
+            }
+            else
+            {
+                health.ChangeHealth(amountToChangeStat);
+                return true;
+            }
+            health.ChangeHealth(amountToChangeStat);
+        }
+        return false;
+
+    }
+
+    public enum StatToChange
+    {
+        none,
+        health,
+        shield,
+        damage,
+        stamina
+    };
+
+    public enum AttributeToChange
+    {
+        none,
+        speed,
+        strenght,
+        agility
+        
+    };
+}
